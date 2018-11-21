56bfe1fec722121ce3c480c95b8a9d0e8edb03db
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-11-20 23:10:36 -0800

    rename

--- pochicomp.sh
+++ pochicomp.sh
@@ -1,2 +1,2 @@
 rm compare_result/comp_result.csv
-java -jar ./pochi_new/pochi-cli/target/pochi-runner-1.0-SNAPSHOT.jar ./compare_new.js ./search_result/a.csv ./search_result/b.csv > ./compare_result/comp_result.csv
+java -jar ./pochi_new/pochi-cli/target/pochi-cli-1.0-SNAPSHOT.jar ./compare_new.js ./search_result/a.csv ./search_result/b.csv > ./compare_result/comp_result.csv
