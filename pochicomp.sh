6e380428d9b632436fe290d8c07f4c9589f42114
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-11-22 23:02:46 +0900

    fix typo

--- pochicomp.sh
+++ pochicomp.sh
@@ -1,3 +1,3 @@
 rm compare_result/comp_result.csv
 export JAVA_HOME=`/usr/libexec/java_home -v "11"`
-java -jar ./pochi_new/pochi-runner/target/pochi-runner-1.0-SNAPSHOT.jar ./compare_new.js ./search_result/a.csv ./search_result/b.csv > ./compare_result/comp_result.csv
+java -jar ./pochi_new/pochi-cli/target/pochi-cli-1.0-SNAPSHOT.jar ./compare_new.js ./search_result/a.csv ./search_result/b.csv > ./compare_result/comp_result.csv
